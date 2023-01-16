9cbd97f9248a8bc5c9810e1f87001a7f577846d1
SeanQuinn <Seanquinn917@gmail.com>
2023-01-16 12:58:42 -0500

    commit tree c97e199cc8d39a4d965ec66215073abe11f994db

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.querySelector('input').value)
+    form.reset()
+  })
+
+})
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = todo
+  document.querySelector('#list').appendChild(p)
+}
+
+function colorChange(color){
+  document.body.style.background = 'violet';
+  
+}
+colorChange()
+
+// function newText(){
+//   for(let item of text){
+//     return item
+//   }
+// }
+document.querySelector('h2').innerText = "Get it DONE"
